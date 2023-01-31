96d6066b36efd7b8056acb434cf3daaf6fb1f1b3
NintenDuck <andrecked507@gmail.com>
2023-01-30 21:10:18 -0600

    Add: Basic Player structure

---
+++ scripts/player.gd
@@ -0,0 +1,13 @@
+extends KinematicBody2D
+
+export var velocity 		= 0
+export var max_velocity 	= 0
+export var friction 		= 0
+export var jump_force 		= 0
+
+func _ready():
+	pass
+
+
+func _physics_process(delta):
+	pass
