98db238ef527d7222e186935616400b6b7d1d94d
NintenDuck <andrecked507@gmail.com>
2023-02-02 19:30:54 -0600

    Add: Basic player state machine & can get inp_dir

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,9 +5,35 @@ export var max_velocity 	= 0
 export var friction 		= 0
 export var jump_force 		= 0
 
-func _ready():
-	pass
+var input_direction = 1
+
+enum states {
+	IDLEING,
+	WALKING,
+	JUMPING,
+	SLIDING,
+}
+
+var current_state = states.IDLEING
+
+func _process(delta):
+	match current_state:
+		states.IDLEING:
+			print(get_input_direction())
+		states.WALKING:
+			print("Walking")
+		states.JUMPING:
+			print("Jumping")
+		states.SLIDING:
+			print("Sliding")
 
 
-func _physics_process(delta):
-	pass
+
+func get_input_direction():
+	""" Esta funcion regresara la actual direccion que dio el usuario """
+	input_direction = int(Input.is_action_pressed("k_right")) - int(Input.is_action_pressed("k_left"))
+	return input_direction
+
+
+# func move():
+	
